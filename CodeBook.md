70845e6d07cd2687c31d72a2d72d3b432171c4a2
YuriyBratko <yuriy.bratko@gmail.com>
2020-06-03 00:19:55 +0300

    Initial commit with script and description files

---
+++ CodeBook.md
@@ -0,0 +1,28 @@
+
+Variables used to produce resulting dataset:
+=============================================
+"X_test": Test set imported from 'test/X_test.txt'
+
+"X_train": Training set imported from 'train/X_train.txt'
+
+"X_full": Combined Test + Training sets
+
+"features": Names of features imported from 'features.txt'
+
+"columnNumbers": row numbers from features variable that correspond to mean and standard deviation measurements. Used to select only the columns needed
+
+"X_mean_std": Set that holds only mean and standard deviation measurements
+
+"y_test": Test labels imported from 'test/y_test.txt'
+
+"y_train": Training labels imported from 'train/y_train.txt'
+
+"y_full": Combined Test + Training labels
+
+"subject_test": Subjects (id's) who performed the activity for Test set, imported from 'test/subject_test.txt'
+
+"subject_train": Subjects (id's) who performed the activity for Training set, imported from 'train/subject_train.txt'
+
+"subject_full": Combined Test + Training Subjects
+
+"Dataset": Set holding mean and standard deviation measurements for each subject for each activity
--- run_analysis.R
+++ run_analysis.R
@@ -1,40 +1,14 @@
-# GettingAndCleaningDataCourseProject
-Course Project for Getting and Cleaning Data Course under Data Science specialization
-
-#Data Transformation flow
-
-## Download and unzip data
-```
+#### Project objective 1 ## Merge the training and the test sets to create one data set.
 ## Download and unzip data for the project
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile ="UCI_HAR_Dataset.zip", method = "curl")
 unzip("UCI_HAR_Dataset.zip")
 ## Remove .zip archive
 file.remove("UCI_HAR_Dataset.zip")
-```
-
-## Test and Train sets are loaded into separate variables and joined using rbind() to form a variable storing full set
-```
 ## Load train and test data sets
 X_test <- read.table("UCI HAR Dataset/test/X_test.txt", quote="\"", comment.char="")
 X_train <- read.table("UCI HAR Dataset/train/X_train.txt", quote="\"", comment.char="")
 ## Merge two data sets
 X_full <- rbind(X_test, X_train)
-```
-
-## Import feature names and use regex to extract colun numbers containing measurements for mean and standard deviation
-```
-## Import features names from 'features.txt'
-features <- read.table("UCI HAR Dataset/features.txt", quote="\"", comment.char="")
-## Rename column name for convinience
-colnames(features)[2]  <- "feature"
-## Extract row numbers with measurement names containing only "mean()" or "std()" using regex
-columnNumbers <- grep("(mean[(][)]|std[(][)])", features$feature)
-## Select columns by numbers returned from previous function using "dplyr" library
-library(dplyr)
-X_mean_std <- select(X_full, all_of(columnNumbers))
-```
-
-
 
 #### Project objective 2 ## Extract only the measurements on the mean and standard deviation for each measurement.
 ## Import features names from 'features.txt'
@@ -93,4 +67,5 @@ Dataset <- X_mean_std %>%
   summarise_all(mean) 
 
 ## Save dataset into .csv
-write.table(Dataset, file = "UCI HAR Dataset/Dataset.csv", sep = ",", row.name=FALSE)
+write.table(Dataset, file = "Dataset.csv", sep = ",", row.name=FALSE)
+
